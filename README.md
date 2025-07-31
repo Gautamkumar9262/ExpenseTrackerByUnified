594dc4d2219ad71680293012f7bbd5b6f8a5a11c
Gautam Kumar <gk779839@gmail.com>
2025-07-31 22:45:41 +0530

    added

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ExpenseTrackerByUnified
 Real time Expense Tracker Application 
+
+
+create .env file in server
+add=>
+PORT = 4000
+MONGODB_URI= mongodb+srv://project1:project1@cluster0.nufowxo.mongodb.net/
+JWT_SECRET="UpFlairs"
--- expense-tracker-main/server/config/mongoDB.js
+++ expense-tracker-main/server/config/mongoDB.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   mongoose.connection.on("connected", () => console.log("database connected"));
 
-  await mongoose.connect(`${process.env.MONGODB_URI}FirstDB`);
+  await mongoose.connect('mongodb+srv://project1:project1@cluster0.nufowxo.mongodb.net/FirstDB');
 };
 
 export default connectDB;
